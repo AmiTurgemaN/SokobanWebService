97aca2efd92ec7503e89680e9dd62b137c947b42
aturgeman <aturgeman@aturgeman-HP.optimaltestcorp.com>
2017-06-27 14:46:41 +0300

    WebService return value fixed
    Changed return value in WebService from String to Response in order to
    handle all the requests and responses in the client side.

--- src/main/java/amit/turgeman/sokoban/servlets/SolutionServlet.java
+++ src/main/java/amit/turgeman/sokoban/servlets/SolutionServlet.java
@@ -9,6 +9,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import amit.turgeman.sokoban.model.Solution;
 import amit.turgeman.sokoban.model.SolutionManager;
@@ -18,45 +19,53 @@ import amit.turgeman.sokoban.model.sokobanSolver.SokobanSolver;
 public class SolutionServlet {
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String getSolutions()
+	public Response getSolutions()
 	{
 		String solutionsString="";
 		SolutionManager sm = new SolutionManager();
-		for(HashMap.Entry<String, String> entry : sm.getSolutionHash().entrySet())
-			solutionsString+=entry.getKey()+":\n"+sm.DecompressSolution(entry.getValue())+"\n\n";
-		return solutionsString;
+		if(sm.getSolutionHash().size()!=0)
+		{
+			for(HashMap.Entry<String, String> entry : sm.getSolutionHash().entrySet())
+				solutionsString+=entry.getKey()+":\n"+sm.DecompressSolution(entry.getValue())+"\n\n";
+			return Response.status(200).entity(solutionsString).build();
+		}
+		else
+		{
+			return Response.status(500).entity("No solutions exist").build();
+		}
+		
 	}
 	
 	@GET
 	@Path("/{levelName}")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String getSolution(@PathParam("levelName") String levelName)
+	public Response getSolution(@PathParam("levelName") String levelName)
 	{
 		levelName=levelName.toLowerCase();
 		SolutionManager sm = new SolutionManager();
 		String compressedSolution = sm.getSolutionHash().get(levelName); 
 		if(compressedSolution!=null)
-			return sm.DecompressSolution(compressedSolution);
-		return "Level Does Not Exist";
+			return Response.status(200).entity(sm.DecompressSolution(compressedSolution)).build();
+		return Response.status(500).entity("Level not found").build();
 	}
 
 	@POST
 	@Path("/{levelName}")
 	@Produces(MediaType.TEXT_PLAIN)
 	@Consumes(MediaType.TEXT_PLAIN)
-	public String getSolution(@PathParam("levelName") String levelName, String levelString)
+	public Response getSolution(@PathParam("levelName") String levelName, String levelString)
 	{
 		SolutionManager sm = new SolutionManager();
 		String compressedSolution = sm.getSolutionHash().get(levelName); 
 		if(compressedSolution!=null)
-			return sm.DecompressSolution(compressedSolution);
+			return Response.status(200).entity(sm.DecompressSolution(compressedSolution)).build();
 		else
 		{
 			SokobanSolver sokobanSolver = new SokobanSolver(levelName);
 			sokobanSolver.loadLevel(levelString);
 			String uncompressedSolution=sokobanSolver.solve();
 			sm.addSolution(new Solution(levelName,uncompressedSolution));
-			return uncompressedSolution;
+			return Response.status(201).entity(uncompressedSolution).build();
 		}
 	}
 
